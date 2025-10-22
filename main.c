d0e89ea584d55912da7785c319d507735bd558e4
MINUSHANIMNATH <minushanimnath6@gmail.com>
2025-10-22 05:40:35 +0530

    first main menu

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void cityMenu();
+void routeMenu();
+
+
+
+// *** Main Program ***
+int main() {
+    int choice;
+    do {
+        printf("\n*** Logistics System ***\n");
+        printf("1. City Management\n");
+        printf("2. Route Management\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1: cityMenu();
+             break;
+            case 2: routeMenu();
+             break;
+            case 3: printf("Exit program.\n");
+             break;
+            default: printf("Invalid choice!\n");
+        }
+    } while (choice != 3);
+
+    return 0;
+}
